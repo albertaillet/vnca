388b03864a6804bb4374f222982b24ba01878eb1
Albert Sund Aillet <albert@sundaillet.com>
2022-10-29 08:32:56 +0200

    added haiku version

--- main.py
+++ main.py
@@ -3,17 +3,19 @@
 import jax.numpy as np
 from jax import jit, vmap, value_and_grad
 from jax import lax, random
+from jax.nn import elu
 from einops import rearrange, reduce, repeat
 from optax import adam, apply_updates
 
-from flax.linen import Module, Sequential, Conv, ConvTranspose, Dense, compact, elu
-from flax.training import train_state
+import haiku as hk
+from haiku import Module, Sequential, Conv2D, Conv2DTranspose, Linear, transform
 
 import io
 import optax
 import requests
 import matplotlib.pyplot as plt
 from PIL import Image, ImageDraw
+from dataclasses import dataclass
 
 # typing
 from jax.numpy import ndarray
@@ -24,96 +26,86 @@ LATENT_SIZE = 16
 # %% Define the neural cellular automata
 
 class Flatten(Module):
-  @compact
   def __call__(self, x: ndarray) -> ndarray:
     return rearrange(x, 'b h w c -> b (h w c)')
-
+  
 
 class Elu(Module):
-  alpha: float
-
-  @compact
   def __call__(self, x: ndarray) -> ndarray:
     return elu(x, alpha=1.0)
 
 
 class Encoder(Module):
-  @compact
   def __call__(self, x: ndarray) -> ndarray:
     return Sequential(
       [
-        Conv(features=32, kernel_size=(5, 5), strides=(1, 1), padding=(2, 2)),
-        Elu(alpha=1.0),
-        Conv(features=64, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        Conv(features=128, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        Conv(features=256, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        Conv(features=512, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
+        Conv2D(output_channels=32, kernel_shape=(5, 5), stride=(1, 1), padding=(2, 2)),
+        Elu(),
+        Conv2D(output_channels=64, kernel_shape=(5, 5), stride=(2, 2), padding=(2, 2)),
+        Elu(),
+        Conv2D(output_channels=128, kernel_shape=(5, 5), stride=(2, 2), padding=(2, 2)),
+        Elu(),
+        Conv2D(output_channels=256, kernel_shape=(5, 5), stride=(2, 2), padding=(2, 2)),
+        Elu(),
+        Conv2D(output_channels=512, kernel_shape=(5, 5), stride=(2, 2), padding=(2, 2)),
+        Elu(),
         Flatten(),
-        Dense(256),
+        Linear(output_size=256),
       ]
     )(x)
 
 
 class LinearDecoder(Module):
-  @compact
   def __call__(self, z: ndarray) -> ndarray:
-    return Dense(2048)(z)
+    return Linear(2048)(z)
 
 
 class BaselineDecoder(Module):
-  @compact
   def __call__(self, z: ndarray) -> ndarray:
     return Sequential(
       [
-        ConvTranspose(features=256, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        ConvTranspose(features=128, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        ConvTranspose(features=64, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        ConvTranspose(features=32, kernel_size=(5, 5), strides=(2, 2), padding=(2, 2)),
-        Elu(alpha=1.0),
-        ConvTranspose(features=1, kernel_size=(5, 5), strides=(1, 1), padding=(4, 4)),
+        Conv2DTranspose(output_channels=256, kernel_shape=(5, 5), stride=(2, 2), padding=((2, 2), (2, 2))),
+        Elu(),
+        Conv2DTranspose(output_channels=128, kernel_shape=(5, 5), stride=(2, 2), padding=((2, 2), (2, 2))),
+        Elu(),
+        Conv2DTranspose(output_channels=64, kernel_shape=(5, 5), stride=(2, 2), padding=((2, 2), (2, 2))),
+        Elu(),
+        Conv2DTranspose(output_channels=32, kernel_shape=(5, 5), stride=(2, 2), padding=((2, 2), (2, 2))),
+        Elu(),
+        Conv2DTranspose(output_channels=1, kernel_shape=(5, 5), stride=(1, 1), padding=((4, 4), (4, 4))),
       ]
     )(z)
 
 
 class Residual(Module):
-  @compact
   def __call__(self, x: ndarray) -> ndarray:
     residual = x
-    x = Conv(256, (1, 1), strides=(1, 1))(x)
-    x = Elu(alpha=1.0)(x)
-    x = Conv(256, (1, 1), strides=(1, 1))(x)
+    x = Conv2D(256, (1, 1), stride=(1, 1))(x)
+    x = Elu()(x)
+    x = Conv2D(256, (1, 1), stride=(1, 1))(x)
     return x + residual
 
 
 class VNCADecoder(Module):
-  @compact
   def __call__(self, z: ndarray) -> ndarray:
     return Sequential(
       [
-        Conv(256, (3, 3), strides=(1, 1)),
+        Conv2D(256, (3, 3), stride=(1, 1)),
         Residual(),
         Residual(),
         Residual(),
         Residual(),
-        Conv(256, (1, 1), stride=(1, 1)),
+        Conv2D(256, (1, 1), stride=(1, 1)),
       ]
     )(z)
 
 
+@dataclass
 class BaselineVAE(Module):
-  def setup(self):
-    self.encoder = Encoder()
-    self.linear_decoder = LinearDecoder()
-    self.decoder = BaselineDecoder()
+  encoder: Encoder
+  linear_decoder: LinearDecoder
+  decoder: BaselineDecoder
 
-  @compact
   def __call__(self, x, z_rng):
     z_params = self.encoder(x)
     mean, logvar = rearrange(z_params, 'b (c p) -> p b c', c=128, p=2)
@@ -122,6 +114,7 @@ class BaselineVAE(Module):
     z = rearrange(z, 'b (h w c) -> b h w c', h=2, w=2, c=512)
     recon_x = self.decoder(z)
     return recon_x, mean, logvar
+
 
 def sample(rng, mu, logvar):
   std = np.exp(0.5 * logvar)
@@ -148,89 +141,25 @@ def load_emoji(emoji: str) -> str:
   return load_image(url)
 
 # %% Load the image
-img = load_emoji('🤡')
+img = load_emoji('🔔')
 plt.imshow(img, cmap='gray')
 plt.show()
 
-# %% Train the model
-vae = BaselineVAE()
-lr = 1e-2
+# %% Initialize the model
+@transform
+def model(x, rng):
+  vae = BaselineVAE(
+      encoder=Encoder(),
+      linear_decoder=LinearDecoder(),
+      decoder=BaselineDecoder(),
+  )
+  return vae(x, rng)
+
+ex_input = rearrange(img, 'h w c -> 1 h w c')
 rng = random.PRNGKey(0)
-params = vae.init(
-  random.PRNGKey(0), 
-  rearrange(img, 'h w c -> 1 h w c'),
-  random.PRNGKey(0)
-)['params']
-state = train_state.TrainState.create(apply_fn=vae.apply, params=params, tx=adam(lr))
+params = model.init(rng, ex_input, rng)
 
-# %% Get a table of the model
-vae.tabulate(
-  random.PRNGKey(0), 
-  rearrange(img, 'h w c -> 1 h w c'),
-  random.PRNGKey(0),
-  console_kwargs = {'force_terminal': False, 'force_jupyter': True}
-)
+# %% Show model table
+print(hk.experimental.tabulate(model)(ex_input, rng))
 
-# %%
-def loss_fn(params, x, rng):
-  recon_x, mean, logvar = vae.apply({'params': params}, x, rng)
-  print(recon_x.shape, x.shape, mean.shape, logvar.shape)
-  recon_loss = np.mean(np.square(recon_x - x))
-  kl_loss = -0.5 * np.mean(1 + logvar - np.square(mean) - np.exp(logvar))
-  return recon_loss + kl_loss
-
-def train_step(state, x, rng):
-  loss, grads = value_and_grad(loss_fn)(state.params, x, rng)
-  state = state.apply_gradients(grads=grads)
-  return state, loss
-
-for epoch in range(100):
-  rng, rng_step = random.split(rng)
-  state, loss = train_step(state, rearrange(img, 'h w c -> 1 h w c'), rng_step)
-  print('Epoch %d, loss %.4f'%(epoch, loss))
-
-
-# %% Train NCA
-n_total_steps = 75
-n_growing_steps = 64
-iterations = 2000
-theta = 0.0
-lr = 2e-3
-
-def loss(params, img, theta):
-  losses = []
-  output = make_seed(TARGET_SIZE)
-  for i in range(n_total_steps):
-    output = nca(params, output, theta)
-    if i >= n_growing_steps:
-      loss = np.mean((output[..., :4] - img)**2)
-      losses.append(loss)
-
-  return np.mean(np.array(losses)), output[..., :4].clip(0, 1)
-
-opt = adam(lr)
-opt_state = opt.init(params)
-for iteration in range(iterations):
-
-  (l, out), grads = value_and_grad(loss, has_aux=True)(params, img, theta)
-
-  def norm(x):
-    return x / (np.linalg.norm(x) + 1e-8)
-  
-  grads = [(norm(dw), norm(db)) for (dw, db) in grads]
-  updates, opt_state = opt.update(grads, opt_state)
-  params = apply_updates(params, updates)
-
-  print(f'Iteration {iteration} Loss {l:.4f}')
-
-# %%
-n = 8
-_, im = loss(params, img, 0)
-plt.imshow(im, cmap='gray')
-plt.axis('off')
-plt.show()
-
-# %%
-plt.imshow(img)
-plt.axis('off')
 # %%
