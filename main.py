17f466e5149696150c5364ba1201a8ff22473da3
Albert Sund Aillet <albert@sundaillet.com>
2022-11-01 16:34:29 +0100

    changed data loading

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-# %% Imports
-
+# Imports
 import equinox as eqx
 import jax.numpy as np
 from jax.random import PRNGKey, split
@@ -8,7 +7,7 @@ from optax import adam, exponential_decay
 import matplotlib.pyplot as plt
 
 from models import BaselineVAE
-from data_loading import load_emoji
+from datasets.mnist import load_mnist
 
 # typing
 from jax import Array, vmap
@@ -18,9 +17,10 @@ from jax.random import PRNGKeyArray
 from optax import GradientTransformation
 
 TARGET_SIZE = 28
-LATENT_SIZE = 16
+MODEL_KEY = PRNGKey(0)
+DATA_KEY = PRNGKey(1)
 
-# %% Define the neural nets
+# %%
 @eqx.filter_value_and_grad
 def loss_fn(model: Module, x: Array, key: PRNGKeyArray) -> float:
     keys = split(key, len(x))
@@ -37,31 +37,25 @@ def make_step(model: Module, x: Array, key: PRNGKeyArray, opt_state: tuple, opti
     model = eqx.apply_updates(model, updates)
     return loss, model, opt_state
 
-
-# %% Load the image
-img = load_emoji('🌗', TARGET_SIZE)
-x = repeat(img, 'h w -> 10 1 h w')
-plt.imshow(img, cmap='gray')
-plt.show()
-
 # %% Create the model
-model_key = PRNGKey(0)
-vae = BaselineVAE(key=model_key)
+vae = BaselineVAE(key=MODEL_KEY)
 
 # %% Train the model
+batch_size = 32
 lr = exponential_decay(3e-5, 60, 0.1, staircase=True)
 opt = adam(lr)
 opt_state = opt.init(eqx.filter(vae, eqx.is_array))
 
+train_data, test_data = load_mnist(batch_size=batch_size, key=DATA_KEY)
+
 n_gradient_steps = 100
-for key in split(model_key, n_gradient_steps):
-    loss, vae, opt_state = make_step(vae, x, key, opt_state, opt)
-    print(loss)
+steps = range(n_gradient_steps)
+train_keys = split(DATA_KEY, n_gradient_steps)
+
+for step, batch, key in zip(steps, train_data, train_keys):
+    loss, vae, opt_state = make_step(vae, batch, key, opt_state, opt)
+    print(step, loss)
 
 # %%
-plt.imshow(vae(x[0], PRNGKey(0))[0][0], cmap='gray')
-plt.show()
 plt.imshow(vae.center()[0], cmap='gray')
 plt.show()
-
-# %%
