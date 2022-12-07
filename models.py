46b711a67abc15314a5579f24c257fe4b6768741
Zigolox <41542666+Zigolox@users.noreply.github.com>
2022-12-08 00:34:01 +0100

    changed to scan

--- models.py
+++ models.py
@@ -1,7 +1,8 @@
-from jax import vmap
+from jax import vmap, jit
 import jax.numpy as np
 from jax.random import split, normal, bernoulli
 from jax.nn import elu, sigmoid
+from jax import lax
 
 from einops import rearrange, repeat
 
@@ -274,8 +275,11 @@ class NonDoublingVNCA(AutoEncoder):
         z = repeat(z, 'c -> c h w', h=32, w=32)
 
         # Apply the NCA steps
-        for _ in range(self.N_nca_steps):
-            z = z + self.step(z)
+        scan_fn = jit(lambda z, _: (lax.add(z, self.step(z)), None))
+        z, _ = lax.scan(scan_fn, z, None, length=self.N_nca_steps)
+        # for _ in range(self.N_nca_steps):
+        #    z = z + self.step(z)
+
         return z
 
     def nca_stages(self, n_channels: int = 1, *, key: PRNGKeyArray) -> Array:
