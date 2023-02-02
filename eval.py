bb37b4af7da45119fc56c3e2d5580b73ab517725
Albert Sund Aillet <albert@sundaillet.com>
2023-02-02 21:03:35 +0100

    moved iwelbo test loss to eval

--- eval.py
+++ eval.py
@@ -36,14 +36,13 @@ get_ipython().run_line_magic('cd', '/kaggle/working/vnca')
 # %%
 from functools import partial
 
-import jax.numpy as np
-from jax.random import PRNGKey, split
+import equinox as eqx
+from jax.random import PRNGKey
 from jax import lax
-from jax import pmap, local_devices
 from einops import rearrange
 
-from data import load_data_on_tpu
-from loss import forward
+from data import get_data
+from loss import iwae_loss
 from models import AutoEncoder, BaselineVAE, DoublingVNCA, NonDoublingVNCA
 from log_utils import restore_model
 
@@ -53,39 +52,64 @@ from jax.random import PRNGKeyArray
 
 MODEL_KEY = PRNGKey(0)
 TEST_KEY = PRNGKey(2)
-
-
-# %%
-# Load and restore model
-vnca_model = BaselineVAE(key=MODEL_KEY, latent_size=256)
-vnca_model = restore_model(vnca_model, 'BaselineVAE_gstep100000.eqx', run_path='dladv-vnca/vnca/runs/h8xyupys')
+K = 128
+BATCH_SIZE = 8
 
 
 # %%
 # function to compute the IWELBO loss on the test set
-def test_iwelbo(key: PRNGKeyArray, x: Array, model: AutoEncoder, M: int):
-    key, subkey = split(key)
-    loss = forward(model, x, subkey, M=M)
-    return key, np.mean(loss)
+@partial(eqx.filter_pmap, axis_name='nd', args=(0, None, None, None))
+def test_iwelbo(batched_data: Array, model: AutoEncoder, key: PRNGKeyArray, K: int):
+    return lax.pmean(lax.map(partial(iwae_loss, model, key=TEST_KEY, K=K), batched_data).mean(), 'nd')
 
 
 # %%
-# Load the test set on the TPU
-_, test_data = load_data_on_tpu(devices=local_devices(), dataset='binarized_mnist', key=TEST_KEY)
-test_data_tpu = rearrange(test_data, 't (b n) c h w -> n t b c h w', b=10)
-test_data_tpu.shape
+_, test_data = get_data('binarized_mnist')
+batched_data = rearrange(test_data, "(p k b) c h w -> p k b c h w", p=8, b=5)
+batched_data.shape
 
 
 # %%
-# Get the IWELBO loss on the test set
-_, losses = lax.scan(
-    pmap(partial(test_iwelbo, model=vnca_model, M=128), axis_name='num_devices'), 
-    split(TEST_KEY, 8),
-    test_data_tpu
-)
+model = BaselineVAE(key=MODEL_KEY, latent_size=256)
+model = restore_model(model, 'BaselineVAE_gstep100000.eqx', run_path='dladv-vnca/vnca/runs/h8xyupys')
+params, static = eqx.partition(model, eqx.is_array)
 
 
 # %%
-# Print the IWELBO loss
-np.mean(losses)
+test_iwelbo(batched_data, model=model, key=TEST_KEY, K=128)[0]
+get_ipython().run_line_magic('time', 'test_iwelbo(batched_data,model=model,key=TEST_KEY,K=128)[0]')
 
+
+# %%
+_ = eqx.filter_jit(model)(test_data[0], key=TEST_KEY)
+get_ipython().run_line_magic('time', '_ = eqx.filter_jit(model)(test_data[0],key=TEST_KEY)')
+
+
+# %%
+model = DoublingVNCA(key=MODEL_KEY, latent_size=256)
+model = restore_model(model, 'DoublingVNCA_gstep100000.eqx', run_path='dladv-vnca/vnca/runs/14c2aulc')
+
+
+# %%
+test_iwelbo(batched_data, model=model, key=TEST_KEY, K=128)[0]
+get_ipython().run_line_magic('time', 'test_iwelbo(batched_data,model=model,key=TEST_KEY,K=128)[0]')
+
+
+# %%
+_ = eqx.filter_jit(model)(test_data[0], key=TEST_KEY)
+get_ipython().run_line_magic('time', '_ = eqx.filter_jit(model)(test_data[0],key=TEST_KEY)')
+
+
+# %%
+model = NonDoublingVNCA(key=MODEL_KEY, latent_size=128)
+model = restore_model(model, "NonDoublingVNCA_gstep100000.eqx", run_path="dladv-vnca/vnca/runs/1mmyyzbu")
+
+
+# %%
+test_iwelbo(batched_data, model=model, key=TEST_KEY, K=128)[0]
+get_ipython().run_line_magic('time', 'test_iwelbo(batched_data,model=model,key=TEST_KEY,K=128)[0]')
+
+
+# %%
+_ = eqx.filter_jit(model)(test_data[0], key=TEST_KEY)
+get_ipython().run_line_magic('time', '_ = eqx.filter_jit(model)(test_data[0],key=TEST_KEY)')
